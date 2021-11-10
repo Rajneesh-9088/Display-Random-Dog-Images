78c5a686843a772c2ddbd8c66b1abe3225fce0e4
Rajneesh-9088 <rp8735685@gmail.com>
2021-11-10 20:00:58 +0530

    custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const ErrorResponse = require('../utils/errorReponse')
 exports.register = async (req,res,next) =>{
 
     const {username, email, password} = req.body;
@@ -18,10 +18,7 @@ exports.register = async (req,res,next) =>{
              user,
         })
     } catch(error) {
-         res.status(500).json({
-              success: false,
-              error: error.message
-         })
+        next(error)
     }
 }
 
@@ -29,22 +26,19 @@ exports.login =  async (req,res,next) => {
     const {email,password} = req.body;
 
     if(!email || !password) {
-         res.status(400).json({success: false, error: "Please provide email and password"})
+         return next(new ErrorResponse("Please Provide an email and password",400))
     }
 
     try {
         const user = await User.findOne({email}).select("+password");
         if(!user){
-             res.status(404).json({success:false, error: "Invalid credentials"})
+            return next(new ErrorResponse("Invalid Credentials",401))
         }
 
         const isMatch = await user.matchPasswords(password)
 
         if(!isMatch){
-             res.status(404).json({
-                 success: false,
-                 error: "Invalid credentials"
-             })
+            return next(new ErrorResponse("Invalid Credentials",401))
         }
 
         res.status(200).json({
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config({path: "./config.env"});
 const express  = require('express');
 const connectDB = require('./config/db');
 const connectDb = require('./config/db');
+const errorHandler = require('./middleware/error');
+const errorHanldler = require('./middleware/error');
 
 // ConnecteDB
 connectDB()
@@ -12,6 +14,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
+
+// last middlewarre 
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 
